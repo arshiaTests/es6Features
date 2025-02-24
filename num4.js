cb0fe59f102c2ef4cd2f6c2dbb9b93acc510deea
arshiaTests <arshia.org79@gmail.com>
2025-02-24 21:06:15 +0330

    num 4 good

--- num4.js
+++ num4.js
@@ -1,5 +1,18 @@
-//4.default parameters : set default values in functions
-function some (name = "arshia"){
-    console.log(`hello ${name}`)
-}
-some()
+// نمایش اطلاعات کاربر
+const showUserInfo = (name = "ناشناس", age = "نامشخص", city = "نامشخص") => {
+    console.log(`
+    اطلاعات کاربر:
+    - نام: ${name}
+    - سن: ${age}
+    - شهر: ${city}
+    `);
+  };
+  showUserInfo("عرشیا", 25, "تهران");
+  showUserInfo(); 
+  
+//تابع محاسبه حقوق کارمند 
+const calculateSalary = (baseSalary, bonus = 0) => {
+    const totalSalary = baseSalary + bonus;
+    console.log(`حقوق کل: ${totalSalary} تومان`);
+  };
+  calculateSalary(5000000, 1000000); // حقوق پایه و پاداش وارد شده است
