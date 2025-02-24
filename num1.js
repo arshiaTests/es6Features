ac10364a6f8bc884ca8b301e4275efada737646c
arshiaTests <arshia.org79@gmail.com>
2025-02-24 20:40:47 +0330

    tamrin 1

--- num1.js
+++ num1.js
@@ -1,5 +1,18 @@
-//1 let&const
-let name = "arshia"
+let name = "arshia";
 const age = 30;
-//let : block-scoped variable.
-//const : immutable variable.
+
+console.log(name); // خروجی: arshia
+console.log(age); // خروجی: 30
+
+name = "ali"; // تغییر مقدار متغیر let
+console.log(name); // خروجی: ali
+
+// age = 31; // این خط باعث خطا می‌شود، زیرا const غیرقابل تغییر است.
+// console.log(age); // خطا: Assignment to constant variable.
+
+if (true) {
+  let city = "Tehran";
+  console.log(city); // خروجی: Tehran
+}
+
+// console.log(city); // این خط باعث خطا می‌شود، زیرا city فقط در بلوک if تعریف شده است.
