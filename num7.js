ad12ed6d5dd83ad23fa97631b74a705c7c9d905c
arshiaTests <arshia.org79@gmail.com>
2025-02-25 15:37:38 +0330

    do it like pro

--- num7.js
+++ num7.js
@@ -1,5 +1,13 @@
-//Rest operator (...):collects multiple values into an array.
-function sum(...nums){
-    return nums.reduce((acc , val)=> acc + val , 0)
-}
-console.log(sum(1,2,3))
+// 1. محاسبه مجموع اعداد
+const sum = (...nums) => {
+    return nums.reduce((acc, val) => acc + val, 0);
+  };
+  console.log("مجموع اعداد:", sum(1, 2, 3, 4, 5)); 
+
+  // 2. محاسبه میانگین اعداد
+const average = (...nums) => {
+    const total = nums.reduce((acc, val) => acc + val, 0);
+    return total / nums.length;
+  };
+  console.log("میانگین اعداد:", average(10, 20, 30, 40)); 
+  
