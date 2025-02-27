ec0db14525350d8aa0795bb2007fbb4f5b487366
arshiaTests <arshia.org79@gmail.com>
2025-02-27 14:31:53 +0330

    num10

--- num10.js
+++ num10.js
@@ -1,5 +1,8 @@
-//10 modules :import & export code 
-export const greet =()=>console.log("hello!");
-import {greet} from "./module.js";
-greet()
-//search for more 
+// صادر کردن توابع و متغیرها
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+export const PI = 3.14;
+
+// صادر کردن پیش‌فرض
+const multiply = (a, b) => a * b;
+export default multiply;
