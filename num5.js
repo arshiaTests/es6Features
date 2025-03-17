d9c4bf20b37022bcaba3f473f03537e94db04b82
arshiaTests <arshia.org79@gmail.com>
2025-03-17 18:32:07 +0330

    update num 5

--- num5.js
+++ num5.js
@@ -19,3 +19,37 @@ console.log(namee, agee); // arshia 30
 
 //تمرین 3: Destructuring با تغییر نام متغیرها
 
+
+const userrr = { name: "arshia", age: 25 };
+const { name: fullName, age: userAge } = userrr;
+console.log(fullName, userAge); // arshia 25
+
+//تمرین 4: Destructuring در توابع
+
+const printUserInfo = ({ name, age }) => {
+    console.log(`Name: ${name}, Age: ${age}`);
+};
+
+const u = { name: "arshia", age: 25 };
+printUserInfo(u); // Name: arshia, Age: 25
+
+//تمرین 5: Destructuring با آرایه‌های تو در تو
+
+const numbersss = [1, [2, 3], 4];
+const [firsttt, [seconddd, thirddd], fourth] = numbersss;
+console.log(firsttt, seconddd, thirddd, fourth); // 1 2 3 4
+
+//تمرین 6: Destructuring با اشیاء تو در تو
+
+const userrrr = {
+    name: "arshia",
+    age: 25,
+    address: {
+        city: "Tehran",
+        country: "Iran"
+    }
+};
+
+const { naame, aage, address: { city, country } } = userrrr;
+console.log(name, age, city, country); // arshia 25 Tehran Iran
+
