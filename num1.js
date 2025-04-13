e8b128092446b7c54374f245943df9456b9e4092
arshiaTests <arshia.org79@gmail.com>
2025-04-13 18:33:56 +0330

    update

--- num1.js
+++ num1.js
@@ -1,7 +1,7 @@
 /////////1 let&const
 let name = "arshia"
 const age = 30;
-///let : block-scoped variable.
+////let : block-scoped variable.
 ///const : immutable variable..
 
 /// تمرین ۱: متغیرهای ساده
