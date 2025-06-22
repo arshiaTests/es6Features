9393b570d5a2b8626becbdb80c8377ec9f09601b
arshiaTests <arshia.org79@gmail.com>
2025-06-22 23:44:42 +0330

    wae

--- num2.js
+++ num2.js
@@ -11,7 +11,7 @@ const power = (base, exp) => Math.pow(base, exp);
 const greeting = name => `سلام ${name}!`;
 const fullName = (firstName, lastName) => `${firstName} ${lastName}`;
 
-//////تمرین ۳: توابع با منطق شرطی
+///////تمرین ۳: توابع با منطق شرطی
 const isAdult = age => age >= 18 ? "بزرگسال" : "کودک";
 const getGrade = score => {
     if (score >= 90) return "A";
